d4df302f102274352b0c7f792223f896fd4ce76a
Bernhard Knasmueller <e1109965@student.tuwien.ac.at>
2017-03-06 14:47:39 +0100

    made ip configuration easier
    infrastructure ip is now the ip where mysql, redis and rabbitmq are running

---
+++ src/main/java/ac/at/tuwien/infosys/visp/runtime/configuration/SpringRedisConfig.java
@@ -0,0 +1,42 @@
+package ac.at.tuwien.infosys.visp.runtime.configuration;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class SpringRedisConfig {
+
+    @Value("${visp.infrastructure.ip}")
+    private String infrastructureIp;
+
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName(infrastructureIp);
+        connectionFactory.setPort(6379);
+        return connectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate() {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory());
+        return redisTemplate;
+    }
+
+    @Bean
+    public StringRedisTemplate strRedisTemplate() {
+        StringRedisTemplate redisTemplate = new StringRedisTemplate();
+        redisTemplate.setConnectionFactory(connectionFactory());
+        return redisTemplate;
+    }
+}
