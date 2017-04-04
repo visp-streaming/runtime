b8e973d8b5911a5dfea580a86027a34ce555c45a
Christoph Hochreiner <ch.hochreiner@gmail.com>
2017-04-04 15:19:20 +0200

    extended operator API

--- src/main/java/ac/at/tuwien/infosys/visp/runtime/restAPI/OperatorAPI.java
+++ src/main/java/ac/at/tuwien/infosys/visp/runtime/restAPI/OperatorAPI.java
@@ -1,13 +1,20 @@
 package ac.at.tuwien.infosys.visp.runtime.restAPI;
 
+import ac.at.tuwien.infosys.visp.common.operators.Operator;
+import ac.at.tuwien.infosys.visp.common.operators.ProcessingOperator;
 import ac.at.tuwien.infosys.visp.common.resources.OperatorConfiguration;
 import ac.at.tuwien.infosys.visp.runtime.configuration.OperatorConfigurationBootstrap;
+import ac.at.tuwien.infosys.visp.runtime.datasources.ProcessingDurationRepository;
+import ac.at.tuwien.infosys.visp.runtime.datasources.entities.ProcessingDuration;
 import ac.at.tuwien.infosys.visp.runtime.monitoring.ResourceUsage;
+import ac.at.tuwien.infosys.visp.runtime.topology.TopologyManagement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/operators")
 @RestController
@@ -16,22 +23,73 @@ public class OperatorAPI {
     @Autowired
     private ResourceUsage resourceUsage;
 
+    @Autowired
+    private TopologyManagement topologymgmt;
+
+    @Autowired
+    private ProcessingDurationRepository pcdr;
+
     @RequestMapping(value = {"/getOperatorConfiguration/type/{operatorType}"}, method = RequestMethod.GET)
     public OperatorConfiguration getOperatorConfigurationForOperatorType(@PathVariable String operatorType) {
+        OperatorConfiguration opconfig =  new OperatorConfiguration(operatorType, 2400);
 
-        OperatorConfiguration op =  new OperatorConfiguration(operatorType, 2400);
-        op.setPlannedResources(new OperatorConfigurationBootstrap(operatorType).getExpected());
-        op.setActualResources(resourceUsage.calculatelatestActualUsageForOperatorType(operatorType));
-        return op;
+        for (Operator operator : topologymgmt.getTopology().values()) {
+            if (operator.getType().equals(operatorType)) {
+                if (operator instanceof ProcessingOperator) {
+                    opconfig.setExpectedDuration(((ProcessingOperator) operator).getExpectedDuration());
+                    break;
+                }
+            }
+        }
+
+        List<ProcessingDuration> pds = pcdr.findFirst5ByOperatorOrderByIdDesc(operatorType);
+        Integer counter = 0;
+        Double duration = 0.0;
+        for (ProcessingDuration pd : pds) {
+            duration+=pd.getDuration();
+        }
+
+        if (counter == 0) {
+            opconfig.setActualDuration(opconfig.getExpectedDuration());
+        } else {
+            opconfig.setActualDuration(duration/counter);
+        }
+
+        opconfig.setPlannedResources(new OperatorConfigurationBootstrap(operatorType).getExpected());
+        opconfig.setActualResources(resourceUsage.calculatelatestActualUsageForOperatorType(operatorType));
+        return opconfig;
     }
 
     @RequestMapping(value = {"/getOperatorConfiguration/name/{operatorName}"}, method = RequestMethod.GET)
     public OperatorConfiguration getOperatorConfiguration(@PathVariable String operatorName) {
 
-        OperatorConfiguration op =  new OperatorConfiguration(operatorName, 2400);
-        op.setPlannedResources(new OperatorConfigurationBootstrap(operatorName).getExpected());
-        op.setActualResources(resourceUsage.calculatelatestActualUsageForOperatorid(operatorName));
-        return op;
+        OperatorConfiguration opconfig =  new OperatorConfiguration(operatorName, 2400);
+
+        for (Operator operator : topologymgmt.getTopology().values()) {
+            if (operator.getName().equals(operatorName)) {
+                if (operator instanceof ProcessingOperator) {
+                    opconfig.setExpectedDuration(((ProcessingOperator) operator).getExpectedDuration());
+                    break;
+                }
+            }
+        }
+
+        List<ProcessingDuration> pds = pcdr.findFirst5ByOperatorOrderByIdDesc(operatorName);
+        Integer counter = 0;
+        Double duration = 0.0;
+        for (ProcessingDuration pd : pds) {
+            duration+=pd.getDuration();
+        }
+
+        if (counter == 0) {
+            opconfig.setActualDuration(opconfig.getExpectedDuration());
+        } else {
+            opconfig.setActualDuration(duration/counter);
+        }
+
+        opconfig.setPlannedResources(new OperatorConfigurationBootstrap(operatorName).getExpected());
+        opconfig.setActualResources(resourceUsage.calculatelatestActualUsageForOperatorid(operatorName));
+        return opconfig;
     }
 
 
