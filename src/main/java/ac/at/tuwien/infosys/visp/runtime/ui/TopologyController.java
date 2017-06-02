caddfa985d9bc025d6c6e07a0a82470a9a58eeeb
thiessl <thomashiessl@gmx.at>
2017-06-03 00:30:43 +0200

    topology controller updated

--- src/main/java/ac/at/tuwien/infosys/visp/runtime/ui/TopologyController.java
+++ src/main/java/ac/at/tuwien/infosys/visp/runtime/ui/TopologyController.java
@@ -33,7 +33,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 @Controller
 @DependsOn("configurationprovider")
