66e96ef817b5105549273dacf9591471a6193b2d
Christoph Hochreiner <ch.hochreiner@gmail.com>
2017-06-16 10:59:59 +0200

    implemented availability watchdog

--- src/main/java/ac/at/tuwien/infosys/visp/runtime/monitoring/AvailabilityWatchdog.java
+++ src/main/java/ac/at/tuwien/infosys/visp/runtime/monitoring/AvailabilityWatchdog.java
@@ -1,20 +1,120 @@
 package ac.at.tuwien.infosys.visp.runtime.monitoring;
 
 
+import ac.at.tuwien.infosys.visp.common.operators.Operator;
+import ac.at.tuwien.infosys.visp.common.operators.ProcessingOperator;
+import ac.at.tuwien.infosys.visp.runtime.configuration.Configurationprovider;
+import ac.at.tuwien.infosys.visp.runtime.datasources.DockerContainerRepository;
+import ac.at.tuwien.infosys.visp.runtime.datasources.RuntimeConfigurationRepository;
+import ac.at.tuwien.infosys.visp.runtime.datasources.entities.DockerContainer;
+import ac.at.tuwien.infosys.visp.runtime.datasources.entities.RuntimeConfiguration;
+import ac.at.tuwien.infosys.visp.runtime.reasoner.ReasonerUtility;
+import ac.at.tuwien.infosys.visp.runtime.resourceManagement.DockerContainerManagement;
+import ac.at.tuwien.infosys.visp.runtime.resourceManagement.ProcessingNodeManagement;
+import ac.at.tuwien.infosys.visp.runtime.topology.TopologyManagement;
+import ac.at.tuwien.infosys.visp.runtime.topology.TopologyUpdateHandler;
+import ac.at.tuwien.infosys.visp.runtime.utility.Utilities;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.net.URL;
 
 @Service
 public class AvailabilityWatchdog {
 
     private static final Logger LOG = LoggerFactory.getLogger(AvailabilityWatchdog.class);
 
+    @Autowired
+    private TopologyManagement topologyMgmt;
+
+    @Autowired
+    private Configurationprovider config;
+
+    @Autowired
+    private DockerContainerManagement dcm;
+
+    @Autowired
+    private DockerContainerRepository dcr;
+
+    @Autowired
+    private ProcessingNodeManagement pcm;
+
+    @Autowired
+    private ReasonerUtility reasonerUtility;
+
+    @Autowired
+    private RuntimeConfigurationRepository rcr;
+
+    @Autowired
+    private TopologyUpdateHandler topologyUpdateHandler;
+
+    @Autowired
+    private Utilities utilities;
+
+    @Scheduled(fixedRateString = "${visp.checkavilability.period}")
     public void checkAvailablitiyOfContainer() {
-        //TODO implement me: iterate through all docker container and check if they are available
+        for (Operator op : topologyMgmt.getOperatorsForAConcreteLocation(config.getRuntimeIP())) {
+            if (op instanceof ProcessingOperator) {
+                for (DockerContainer dc : dcr.findByOperatorNameAndStatus(op.getName(), "running")) {
+                    if (!dcm.checkIfContainerIsRunning(dc)) {
+                        LOG.info("Container \"" + dc.getContainerid() + "\" for operator \"" + op.getName() + "\" could not be reached.");
+                        String compensation = ((ProcessingOperator) op).getCompensation();
+                        if (compensation == null) {
+                            compensation = "redeploySingle";
+                        }
+                        if (compensation.equals("redeploySingle")) {
+                            dcm.removeContainer(dc);
+                            try {
+                                pcm.scaleup(reasonerUtility.selectSuitableDockerHost(op), op);
+                                LOG.info("Redeployed container for operator \"" + op.getName() + "\".");
+                            } catch (Exception e) {
+                                LOG.error(e.getMessage());
+                            }
+                            continue;
+                        }
 
+                        if (compensation.equals("redeployTopology")) {
+                            try {
+                                RuntimeConfiguration rc = rcr.findFirstByKey("last_topology_file");
+                                utilities.clearAll();
+                                topologyUpdateHandler.handleUpdateFromUser(rc.getValue());
+                                LOG.info("Redeployed complete topology.");
+                            } catch (Exception e) {
+                                LOG.error(e.getMessage());
+                            }
+                            continue;
+                        }
+
+                        if (compensation.startsWith("mailto:")) {
+                            try {
+                                compensation.replace("mailto:", "")
+                                //TODO send mail
+                                LOG.info("Redeployed complete topology.");
+                            } catch (Exception e) {
+                                LOG.error(e.getMessage());
+                            }
+                            continue;
+                        }
+
+                        if (compensation.startsWith("deploy:")) {
+                            try {
+                                URL topology = new URL(compensation.replace("deploy:", ""));
+                                String newTopology = IOUtils.toString(IOUtils.toByteArray(topology), "UTF-8");
+                                topologyUpdateHandler.handleUpdateFromUser(newTopology);
+                                LOG.info("Deployed new topology from URI.");
+                            } catch (Exception e) {
+                                LOG.error(e.getMessage());
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-
 }
 
 
