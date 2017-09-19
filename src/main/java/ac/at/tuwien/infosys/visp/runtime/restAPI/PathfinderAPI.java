f82b3ef937822e8060f9f9d7be5c04b90e065c20
Bernhard Knasmueller <e1109965@student.tuwien.ac.at>
2017-09-19 12:45:14 +0200

    create REST API to be used by Pathfinder

---
+++ src/main/java/ac/at/tuwien/infosys/visp/runtime/restAPI/PathfinderAPI.java
@@ -0,0 +1,48 @@
+package ac.at.tuwien.infosys.visp.runtime.restAPI;
+
+import ac.at.tuwien.infosys.visp.runtime.topology.TopologyManagement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class is the interface to the Pathfinder Fault Tolerance Framework
+ */
+
+@RequestMapping("/pathfinder")
+@RestController
+public class PathfinderAPI {
+
+    @Autowired
+    TopologyManagement topologyManagement;
+
+    private static final Logger LOG = LoggerFactory.getLogger(PathfinderAPI.class);
+
+    @RequestMapping("/getAllStatistics")
+    @ResponseBody
+    public Map<String, Object> getAllStatistics(HttpServletRequest request) {
+        Map<String, Object> jsonData = new HashMap<>();
+
+        for(String operator: topologyManagement.getOperatorsAsList()) {
+            HashMap<String, Object> hm = new HashMap<>();
+
+            hm.put("cpu_now", 0.8);
+            hm.put("ram_now", 0.6);
+            hm.put("network_out", 110);
+
+            jsonData.put(operator, hm);
+        }
+
+        LOG.debug("/getAllStatistics request from IP " + request.getRemoteAddr());
+        return jsonData;
+    }
+}
